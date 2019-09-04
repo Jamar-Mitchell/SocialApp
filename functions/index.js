263a96fc9f23f98e200efa5cccde25ce89253dea
Jamar-Mitchell <jamar-mitchell@hotmail.com>
2019-09-04 13:55:28 -0400

    setting up firebase

---
+++ functions/index.js
@@ -0,0 +1,57 @@
+const functions = require("firebase-functions");
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mysocialapp-c68aa.firebaseio.com"
+});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+exports.getPosts = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection("Posts")
+    .get()
+    .then(data => {
+      let posts = [];
+      data.forEach(doc => {
+        posts.push(doc.data());
+      });
+
+      return res.json(posts);
+    })
+    .catch(err => console.error(err));
+});
+
+exports.createPost = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    return req.status(400).json({ error: "Method is not allowed" });
+  }
+
+  const newPost = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin
+    .firestore()
+    .collection("Posts")
+    .add(newPost)
+    .then(doc => {
+      res.json({ message: "document ${doc.id} created succesfully" });
+    })
+    .catch(err => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
